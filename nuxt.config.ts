d6464d83e0c29e543d4f9625d9dacb574be676a4
Maxime Pauvert <maximepvrt@gmail.com>
2025-06-02 21:50:52 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,9 @@ export default defineNuxtConfig({
     ui: {
         colorMode: false
     },
+    content: {
+        preview: {
+            api: 'https://api.nuxt.studio',
+        },
+    },
 })
